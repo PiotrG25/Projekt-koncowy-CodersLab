1a8fbf153d6557c5b266aa7390de1e1d4c7096c4
Piotr <piotrgoralewicz25@gmail.com>
2018-12-10 11:33:08 +0100

    randomIndexes to from stream

--- Projekt koncowy/src/main/java/pl/coderslab/beans/RandomMachine.java
+++ Projekt koncowy/src/main/java/pl/coderslab/beans/RandomMachine.java
@@ -1,6 +1,11 @@
 package pl.coderslab.beans;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class RandomMachine {
     public static void main(String[] args) {
@@ -14,7 +19,7 @@ public class RandomMachine {
         int [][] tab = new int [length][];
         for(int i = 0; i < tab.length; i++){
             tab[i] = new int [(int)(Math.random() * 3) + 1];//{1, 2, 3}
-            int [] helpTab = randomIndexes(length, i);
+            Integer [] helpTab = randomIndexes(length, i);
             for(int j = 0; j < tab[i].length; j++){
                 tab[i][j] = helpTab[j];
             }
@@ -23,32 +28,14 @@ public class RandomMachine {
         return tab;
     }
 
-    private static int[] randomIndexes(int length, int otherThanThis){
-        int [] tab = new int [length];
-        for(int i = 0; i < tab.length; i++){//Wypełniamy tablicę tab indeksami
-            tab[i] = i;
-        }
-        for(int i = 0; i < tab.length; i++){//Usunięcie niepotrzebnego indexu na koniec
-            if(tab[i] == otherThanThis){
-                int help = tab[i];
-                tab[i] = tab[tab.length - 1];
-                tab[tab.length - 1] = help;
-                break;
-            }
-        }
+    private static Integer[] randomIndexes(int length, int otherThanThis){
+        List<Integer> list = IntStream
+                .range(0, length)
+                .filter(el -> el != otherThanThis)
+                .boxed()
+                .collect(toList());
 
-        int [] tab2 = new int [length - 1];//Tworzymy nową tablicę i długość pomocniczą
-        int length2 = length - 1;
-
-        for(int i = 0; i < tab2.length; i++){
-            int randomIndex = (int)(Math.random() * length2);
-
-            tab2[i] = tab[randomIndex];
-            tab[randomIndex] = tab[length2 - 1];
-            length2--;
-        }
-
-        return tab2;
+        Collections.shuffle(list);
+        return list.toArray(new Integer [list.size()]);
     }
 }
-
